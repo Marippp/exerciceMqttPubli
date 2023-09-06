dcfe0508148e1cdfecb1734a85ac22425b192e6b
Marippp <marinapatry@hotmail.com>
2023-09-06 13:37:56 -0400

    Exercice terminé et fonctionnel

--- main.py
+++ main.py
@@ -2,21 +2,29 @@
 import RPi.GPIO as GPIO
 import dht11
 import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
 
 instance = dht11.DHT11(pin=17)
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-#publish.single("paho/test/single", "payload", hostname="test.mosquitto.org")
-while(True):
+client = mqtt.Client()
+
+client.on_connect = on_connect
+
+client.connect("10.4.1.42", 1883, 60)
+while True:
     result = instance.read()
     if result.is_valid():
+        client.publish("Marina/Temperature", payload=result.temperature, qos=1, retain=True)
         print("Temperature: %-3.1f C" % result.temperature)
-        print("Humidity: %-3.1f %%" % result.humidity)
     else:
         print("Error: %d" % result.error_code)
-    time.sleep(1)
+    time.sleep(2)
+
+# print("Humidity: %-3.1f %%" % result.humidity)
